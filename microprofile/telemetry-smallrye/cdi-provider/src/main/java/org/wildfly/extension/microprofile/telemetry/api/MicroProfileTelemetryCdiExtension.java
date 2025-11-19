52f6166c14383a7edd49cc769b5955e6fc7d6186
James R. Perkins <jperkins@ibm.com>
2025-11-19 15:07:44 -0800

    [WFLY-20251] Add a filter to the OpenTelemetryHandler added by SmallRye.
    https://issues.redhat.com/browse/WFLY-20251

--- microprofile/telemetry-smallrye/cdi-provider/src/main/java/org/wildfly/extension/microprofile/telemetry/api/MicroProfileTelemetryCdiExtension.java
+++ microprofile/telemetry-smallrye/cdi-provider/src/main/java/org/wildfly/extension/microprofile/telemetry/api/MicroProfileTelemetryCdiExtension.java
@@ -7,8 +7,13 @@ package org.wildfly.extension.microprofile.telemetry.api;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Filter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import io.smallrye.opentelemetry.api.OpenTelemetryConfig;
+import io.smallrye.opentelemetry.api.OpenTelemetryHandler;
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.inject.Default;
 import jakarta.enterprise.inject.spi.AfterBeanDiscovery;
@@ -16,6 +21,7 @@ import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.Extension;
 import jakarta.inject.Singleton;
 import org.eclipse.microprofile.config.Config;
+import org.jboss.logmanager.ExtLogRecord;
 
 public class MicroProfileTelemetryCdiExtension implements Extension {
     private final Map<String, String> serverConfig;
@@ -44,5 +50,27 @@ public class MicroProfileTelemetryCdiExtension implements Extension {
                             return (OpenTelemetryConfig) () -> properties;
                         }
                 );
+        // This is a hack to add a filter to the OpenTelemetryHandler. SmallRye adds this to the root logger in the
+        // CDI producer for the OpenTelemetry instance.
+        final Logger rootLogger = Logger.getLogger("");
+        // Get the handlers and look for the OpenTelemetryHandler
+        for (Handler handler : rootLogger.getHandlers()) {
+            if (handler instanceof OpenTelemetryHandler) {
+                handler.setFilter(ExtLogRecordFilter.INSTANCE);
+            }
+        }
+    }
+
+    private static class ExtLogRecordFilter implements Filter {
+        private static final ExtLogRecordFilter INSTANCE = new ExtLogRecordFilter();
+
+        @Override
+        public boolean isLoggable(final LogRecord record) {
+            if (record instanceof ExtLogRecord extLogRecord) {
+                // Copies MDC, NDC and formats the message
+                extLogRecord.copyAll();
+            }
+            return true;
+        }
     }
 }
